cae87d141f744c0d748b6226e562a4f7b1634bb1
Darwin-Nunez-10 <darwinn990@gmail.com>
2026-04-14 18:14:26 -0600

    Function is already

---
+++ Repositories/IReservationRepository.cs
@@ -0,0 +1,13 @@
+using reservations_api.Models.Entities;
+
+namespace reservations_api.Repositories;
+
+public interface IReservationRepository
+{
+  Task<Reservation> AddAsync(Reservation reservation);
+  Task<List<Reservation>> GetByClassroomAndDateAsync(Guid classroomId, DateOnly date);
+  Task<bool> DeleteAsync(Guid Id); 
+
+  Task<List<Reservation>> GetByDateAsync(DateOnly date);
+
+}
---
+++ Services/IReservationService.cs
@@ -0,0 +1,12 @@
+using reservations_api.DTOs.Requests;
+using reservations_api.DTOs.Responses;
+
+namespace reservations_api.Services;
+
+public interface IReservationService
+{
+  Task<ReservationResponse> CreateAsync(CreateReservationRequest request);
+  Task<bool> DeleteAsync(Guid Id);
+
+  Task<List<ReservationResponse>> GetByDateAsync(DateOnly date);
+}
